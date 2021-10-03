f563a2232073e3f94bcd3b1604c69bc164eb5339
jaykeburger <76579098+jaykeburger@users.noreply.github.com>
2021-10-02 23:19:26 -0500

    Update Main.java

--- src/hackathonproject/Main.java
+++ src/hackathonproject/Main.java
@@ -1,24 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hackathonproject;
 
-/**
- *
- * @author Jayke
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+      public static void main(String args[]) {
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
